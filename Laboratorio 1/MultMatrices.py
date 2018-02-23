ce38a01b04431d521ddc2b41adb4ea34701dccce
anythinglord <sjsalcedoa@unal.edu.co>
2018-02-23 18:07:52 -0500

    Add files via upload

---
+++ Laboratorio 1/MultMatrices.py
@@ -0,0 +1,29 @@
+import time
+import random
+import numpy as np
+
+def Mult(A , B):
+    top = 0
+    t0 = time.clock()
+    mul = 0
+    for i in range(0,tam):
+        for j in range(0,tam):
+            np.dot(A,B)
+        top = time.clock()
+        top = top - t0
+    total = time.clock()
+    return [total,top]
+
+
+tam = input("Tamanio de la matriz: ")
+A = []
+for i in range(0,tam):
+    A.append([])
+    for j in range(0,tam):
+        num = random.randrange(20)
+        A[i].append(num)
+
+totalM = Mult(A, A)
+print "Multiplicacion: "
+print "Tiempo de cada Multiplicacion: " + str(totalM[1])
+print "Tiempo total de la Multiplicacion: " + str(totalM[0])
